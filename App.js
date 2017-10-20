551861973f79d6fbb692170f821d64a5df22ea0d
FCCHC <fccachon@gmail.com>
2017-10-20 11:09:59 -0500

    Change fetch get and post to api.jss file

--- App.js
+++ App.js
@@ -28,123 +28,42 @@ import {
 
 } from 'react-native';
 
-import RNFetchBlob from 'react-native-fetch-blob'
-
-var ImagePicker = require('react-native-image-picker');
+import RadioForm, {RadioButton,
+                   RadioButtonInput,
+                   RadioButtonLabel} from 'react-native-simple-radio-button'
 
 export default class App extends Component<{}> {
   constructor(props){
     super(props);
      this.state = {
-
-       isLoading: true,
-       image: '',
+     value : 0
 
   }
-  this._Post = this._Post.bind(this);
-  this.chooseImage = this.chooseImage.bind(this);
-}
-
- componentDidMount() {
-   console.log('comentario');
-   return fetch('http://192.168.1.40:8000/v2/Sucursales/')
-     .then((response) => response.json())
-     .then((responseJson) => {
-       console.log('responseJson', responseJson);
-       let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-       this.setState({
-         isLoading: false,
-         dataSource: ds.cloneWithRows(responseJson)
-       })
-
-     })
-     .catch((error) => {
-       console.warn(error);
-     });
- }
-
-
- _Post(){
-   console.log('CLICK');
-    path=this.state.image.uri
-  
-     RNFetchBlob.fetch('POST','http://192.168.1.40:8000/v2/update-perfil/',{
-       Authorization : 'Bearer 7e9448ff945688c22e514e7bacf01d619c8061b0',
-       'Content-Type': 'multipart/form-data',
-     }, [
-       {
-         name : 'avatar',
-         filename : 'avatar.jpg',
-         data: RNFetchBlob.wrap(path)
-       },
-       //elements that will be sent as plain text
-       { name : 'token', data :'7e9448ff945688c22e514e7bacf01d619c8061b0'},
-       { name : 'password', data : '54321'},
-       { name : 'nickname', data : 'aortega'},
-       {name : 'descripcion', data : 'descripcion'},
-
-     ]).then((resp)=>{
-          console.log(resp);
-     }).catch((err) => {console.error(err)
-
-   })
 
 }
 
- chooseImage(){
-   ImagePicker.showImagePicker((response)=> {
-     console.log('Response = ', response);
-
-     if(response.didCancel){
-       console.log('User cancelled image picker');
-     }else if(response.error){
-       console.log('ImagePicker error: ', response.error);
-     }
-      else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
-      } else{
-
-        let source ={uri: response.uri.replace('file://', ''), isStatic: true};
-
-        if(Platform.OS === 'android'){
-           source = {uri: response.path, isStatic: true};
-        }
-
-        this.setState({image: source});
-        console.log(this.state.image);
-      }
-   });
- }
 
   render() {
-    if(this.state.isLoading){
-      return (
-        <View style={{flex:1, paddingTop:20}}>
-          <ActivityIndicator />
-        </View>
-      );
-    }
+
+
+    const radio_props = [
+      {label: 'param1', value: 0},
+      {label: 'param2', value: 1}
+    ];
 
     return (
-      <View style={styles.container}>
-        <ListView dataSource={this.state.dataSource}
-                  renderRow={(rowData) => <View>
-                  <Text>{rowData.titulo}</Text>
-                    <Text>{rowData.subtitulo}</Text>
-                    <Text>{rowData.estado}</Text>
-                    <Text> {rowData.longitud}</Text>
-                    <Text>{rowData.latitud}</Text>
-                  </View>}
-                />
-         {this.state.image ? <Image style={{flex:1}} source={this.state.image}></Image>:null}
-          <Button style={styles.button}
-                  title='POST'
-                  onPress={this._Post}/>
+      <View>
 
-          <Button style={styles.button}
-                  onPress={this.chooseImage}
-                  title='PHOTO'/>
+        <Text>PREGUNTA NUMERO UNO</Text>
+        <RadioForm
+         radio_props={radio_props}
+         initial= {0}
+         onPress={(value) => {this.setState({value:value})}}
+         buttonColor={'#f48042'}
+         animation={true}
+       />
       </View>
+
     );
   }
 }
